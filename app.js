199bf1952c0c82a4386e06737a1146ee97c02386
Emmet Tracey <emtracey@tcd.ie>
2018-11-20 13:46:59 +0000

    Minor format changes

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ var crypto    = require('crypto');
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://SenneS_Admin:SenneS2018@sennescluster-onimy.mongodb.net/SenneSDB"
 
-var upc = "012000018794";
+var upc = "721898049963";
 var FridgeID = "sampleFridgeID";
 //getBarcodeInfo(upc, FridgeID);
 getUpdates(FridgeID);
 
 function getBarcodeInfo(barcode) {
 
+    // query for UPCitemdb database
     var query = "https://api.upcitemdb.com/prod/trial/lookup?upc="+barcode;
-    //console.log(query);
 
     const options = {
         url: query,
@@ -23,15 +23,18 @@ function getBarcodeInfo(barcode) {
         }
     };
 
+    // parse the returned object for information we want to input to the DB
     request(options, function(err, res, body) {
         var obj = JSON.parse(body);
         var title = obj.items[0].title;
         var brand = obj.items[0].brand;
         var size = obj.items[0].size;
+        var weight = obj.items[0].weight;
         var barcodeInfo = {
           title: title,
           brand: brand,
-          size: size
+          size: size,
+          weight: weight
         };
         console.log(barcodeInfo);
 
@@ -47,7 +50,7 @@ function getBarcodeInfo(barcode) {
            const collection = client.db("SenneSDB").collection(FridgeID);
 
            // populate the document with information from barcode database
-           var doc = { name: barcodeInfo.title , brand: barcodeInfo.brand, size: barcodeInfo.size };
+           var doc = { name: barcodeInfo.title , brand: barcodeInfo.brand, size: barcodeInfo.size, weight: barcodeInfo.weight };
 
            // insert the document into the DB
            collection.insertOne(doc, function(err, res) {
@@ -98,13 +101,15 @@ function getUpdates(fridgeID) {
      // create the collection object for retrieving documents
      const collection = client.db("SenneSDB").collection(FridgeID);
 
+     // items from the DB are stored in an array of dictionaries
      collection.find().toArray(function(err, items) {
-       console.log(items[0].name);
+       if (err) throw err;
+       console.log("Documents retrieved")
+       console.log(items);
      });
 
      // need to update and return state
      client.close();
-
    });
  }
 
