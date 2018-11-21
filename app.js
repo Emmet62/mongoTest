15b73021443bfa2118e1608737dab7a083a71de9
Emmet Tracey <emtracey@tcd.ie>
2018-11-21 13:06:41 +0000

    Updated three functions

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const request = require('request');
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const randomstring = require("randomstring");
 
 // Connection URL
 const url = "mongodb+srv://SenneS_Admin:SenneS2018@sennescluster-onimy.mongodb.net/SenneSDB";
@@ -23,8 +24,14 @@ MongoClient.connect(url, { useNewUrlParser: true }, function(err, client) {
   // ******************************
   // I had to make the function call from within this connection because I couldn't get it to work outside of it
   // Having an issue where the getBarcodeInfo function would call before the DB connectione was made
-  var upc = "012000018794";
-  getBarcodeInfo(dbClient, upc);
+  var upc = "000054491472";
+  //getBarcodeInfo(dbClient, upc);
+
+  var fridgeID = "testFridge2";
+  var state = 3;
+  //addUpdate(dbClient, fridgeID, state, "sampleString");
+
+  getUpdates(dbClient, fridgeID, state);
 
 });
 
@@ -54,48 +61,63 @@ function getBarcodeInfo(dbClient, barcode) {
          if (err) throw err;
          console.log("Barcodes DB Updated");
 
+        var result = { barcode: barcode, info: barcodeInfo, error: "null"}
+        console.log(result);
+
          // I am not sure how to use callback so can't get it to return the barcodeInfo object
-         //callback(barcodeInfo);
+         //callback(result);
         });
     });
 }
 
 
-function addUpdate(dbClient, item, FridgeID) {
-// update block that is encrypted
-// add fridge_id and increment state
-// need a string
+function addUpdate(dbClient, FridgeID, state, update) {
 // id, state and string
 
-     // create the collection object for inserting documents
-     const collection = dbClient.db("SenneSDB").collection(Fridges);
+   // Create the collection object for inserting documents into Fridges database
+   const collection = dbClient.collection("Fridges");
 
-     var doc = { name: barcodeInfo.title , brand: barcodeInfo.brand, size: barcodeInfo.size, weight: barcodeInfo.weight };
+   // Generate a random string
+   // var randomstring = randomstring.generate(10);
 
-     // insert the document into the DB
-     collection.insertOne(doc, function(err, res) {
-      if (err) throw err;
-      console.log("Document inserted");
+   // *******************************
+   // Not sure how to have a state counter tied to a fridge ID
+   state++;
 
-     // need to update and return state
-     client.close();
-    });
-  }
+   // Create the document containing the ID, state and string
+   var update = { fridge_id: FridgeID , state: state, string: update };
 
-function getUpdates(dbClient, fridgeID, state) {
+   // insert the document into the DB
+   collection.insertOne(update, function(err, res) {
+    if (err) throw err;
+    console.log("Document inserted");
+
+  });
+}
+
+function getUpdates(dbClient, fridgeID, currentState) {
 // Take all updates that are bigger than the state and belong to the fridge
 // return a list of the strings between the two states
 
      // create the collection object for retrieving documents
-     const collection = dbClient.db("SenneSDB").collection(Fridges);
+     const collection = dbClient.collection("Fridges");
 
      // items from the DB are stored in an array of dictionaries
-     collection.find().toArray(function(err, items) {
+     collection.find( { state: { $gt: currentState } }, { fridge_id: fridgeID } ).toArray(function(err, items) {
        if (err) throw err;
        console.log("Documents retrieved")
-       console.log(items);
+
+       var updates = [];
+       for (var i = 0; i < items.length; i++) {
+         updates.push(items[i].string);
+       }
+       console.log(updates);
+
+       currentState++;
+       var result = { new_state: currentState, update: updates, error: null};
+       console.log(result);
      });
 
      // need to update and return state
-     client.close();
+
    }
